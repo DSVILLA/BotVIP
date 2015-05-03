8eb57077ce433ad1b96245f445c47bbb7d22d47e
DSVILLA <equipevilla@live.com>
2015-05-02 23:44:08 -0400

    Update basicBot.js

--- bot-costomiza-o/basicBot.js
+++ bot-costomiza-o/basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/motelbible/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/DSVILLA/BotVIP/master/lang/Langindex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -184,23 +184,23 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/motelbible/basicBot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/DSVILLA/BotVIP/master/bot-costomiza-o/basicBot.js",
         cmdLink: "http://git.io/veGHZ",
-        chatLink: "https://rawgit.com/motelbible/basicBot/master/lang/pt.json",
+        chatLink: "https://rawgit.com/DSVILLA/BotVIP/master/lang/ptdsvilla.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
-            botName: "basicBot",
+            botName: "DEANSE VILLA BOT PREMIUM",
             language: "portuguese",
-            chatLink: "https://rawgit.com/motelbible/basicBot/master/lang/pt.json",
+            chatLink: "https://rawgit.com/DSVILLA/BotVIP/master/lang/ptdsvilla.json",
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
             startupEmoji: false, // true ou false
             cmdDeletion: true,
             maximumAfk: 120,
-            afkRemoval: true,
+            afkRemoval: false,
             maximumDc: 60,
             bouncerPlus: true,
             blacklistEnabled: true,
